90ca08f935e9a580107c67a69d69d9673801ab2d
Julien Mayer <jalexismayer@gmail.com>
2015-06-09 21:28:19 -0500

    include rails helper file

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Signing in' do
+  scenario 'Signing in with correct credentials' do
+    visit '/users/sign_in'
+
+    fill_in 'user[email]', :with => 'user@example.com'
+
+    fill_in 'user[password]', :with => 'password'
+
+    click_on 'Log in'
+
+    expect(page).to have_content 'Sign out'
+  end
+end
