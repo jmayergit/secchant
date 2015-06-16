4d51cafd8488eae953d92314c8fd5f24fdf21b7d
Julien Mayer <jalexismayer@gmail.com>
2015-06-16 16:57:17 -0500

    refactor session spec

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -6,22 +6,22 @@ feature 'User signs in' do
     @user = FactoryGirl.create(:user)
     expect(@user).to be_valid
 
-    visit '/users/sign_in'
-
-    fill_in 'user[email]', :with => @user.email
-    fill_in 'user[password]', :with => @user.password
-    click_on 'Log in'
+    sign_in_with(@user.email, @user.password)
 
     expect(page).to have_content 'Sign out'
   end
 
   scenario 'with invalid credentials' do
-    visit '/users/sign_in'
-
-    fill_in 'user[email]', :with => 'invalid@email.com'
-    fill_in 'user[password]', :with => 'invalid@password.com'
-    click_on 'Log in'
+    sign_in_with('nonregistered@email.com', 'password')
 
     expect(page).to have_content 'Sign in'
+  end
+
+  def sign_in_with(email, password)
+    visit new_user_session_path
+
+    fill_in 'user[email]', :with => email
+    fill_in 'user[password]', :with => password
+    click_on 'Log in'
   end
 end
