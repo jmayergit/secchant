dc26de1850b803c85ab14a5dadaf91c16cdf69de
Julien Mayer <jalexismayer@gmail.com>
2015-07-07 20:24:30 -0500

    add important css and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Forums.create(name: 'SEC Rant')
+Forums.create(name: 'SEC Recruiting')
+Forums.create(name: 'SEC Tickets')
+Forums.create(name: 'Off-Topic Board')
+
+teams = ['Alabama', 'Arkansas', 'Auburn', 'Florida', 'Georgia', 'Kentucky', 'LSU',
+  'Mississippi St.', 'Missouri', 'Ole Miss', 'South Carolina', 'Tennessee',
+  'Texas A&M', 'Vanderbilt']
+
+len = teams.length
+i = 0
+while i < len
+  Forums.create(name: teams[i])
+  i += 1
+end
