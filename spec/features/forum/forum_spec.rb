06ce847f47b8eecee201aa90c8a5d33ca7669864
Julien Mayer <jalexismayer@gmail.com>
2015-07-09 17:03:55 -0500

    you do not test this dumby

--- spec/features/forum/forum_spec.rb
+++ spec/features/forum/forum_spec.rb
@@ -1,20 +1,5 @@
 require 'rails_helper'
 
-feature 'Forum can be accessed' do
-  scenario 'From home page' do
-    visit root_path
-
-    find('#ContentL').click_on 'SEC Rant'
-
-    expect(find('.PgBoardName')).to have_content('SEC Rant')
-  end
-
-  scenario 'From another forums page' do
-    visit forum_show_path(1)
-    expect(find('.PgBoardName')).to have_content 'SEC Rant'
-
-    find('#LNav').click_on 'Help Board'
-
-    expect(current_path).to eq(forum_show_path(2))
-  end
-end
+# feature '' do
+#   scenario ''
+# end
