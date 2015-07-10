12fd1ab7725a7dfbf3ed2ce01bdcd8c957ed3722
Julien Mayer <jalexismayer@gmail.com>
2015-07-10 11:49:46 -0500

    add link to helpers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def home
+    @rant = Forum.find_by name:'SEC Rant'
+    @recruiting = Forum.find_by name:'SEC Recruiting'
+    @tickets = Forum.find_by name:'SEC Tickets'
+    @offtopic = Forum.find_by name:'Off-Topic Board'
+    @help = Forum.find_by name:'Help Board'
   end
 end
