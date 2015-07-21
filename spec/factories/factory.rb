c1f8a586003ef19fba03c2ad505f739c843f6a13
Julien Mayer <jalexismayer@gmail.com>
2015-07-21 14:44:00 -0500

    adds topic and post resources

--- spec/factories/factory.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :forum do
-    name 'Example Forum'
-  end
-end
---
+++ spec/features/topic/topic.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+feature 'Topic' do
+  scenario 'User can create a topic'
+end
