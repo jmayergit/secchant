c77499889733293bf62811c223bd1b76cfd8a457
Julien Mayer <jalexismayer@gmail.com>
2015-08-03 15:56:23 -0500

    adds essential css

--- app/assets/javascripts/forums.js
+++ app/assets/javascripts/forums.js
@@ -1,22 +1,3 @@
-// $(document).ready(function() {
-//   $('.ui.sticky')
-//     .sticky({
-//       context: '#bodyR',
-//       onStick: function() {
-//         console.log('Stuck!')
-//       },
-//       observeChanges: true
-//     })
-//   ;
-//   $('.PgBoardName')
-//     .transition('pulse')
-//   ;
-// });
-// $('.ui.sticky')
-//   .sticky({
-//     context: '#bodyR'
-//   })
-// ;
 $(document).on('ready page:load', function(e) {
   $('.ui.sticky')
     .sticky({
