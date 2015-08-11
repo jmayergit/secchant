d4144a67f8f8c6278ea566d9b3edf70a36610a92
Julien Mayer <jalexismayer@gmail.com>
2015-08-10 20:46:47 -0500

    adds vertical alignment to topic show

--- app/assets/javascripts/forums.js
+++ app/assets/javascripts/forums.js
@@ -6,14 +6,68 @@ $(document).on('ready page:load', function(e) {
     })
   ;
 
-  $('button').click(function(e) {
-    var current_text = $("#" + textAreaId())[0].value;
+  $("button[id='link']").click(function(e) {
+    var current_msg = $('textarea').get(0).value;
 
+    var link = prompt("Enter a valid URL: (must include http://):");
+
+    var new_msg = current_msg + "";
   })
 
-  function textAreaId() {
-    var textarea = $('textarea')[0]
+  $("button[id='img']").click(function(e) {
+    var current_msg = $('textarea').get(0).value;
 
-    return textarea.id
-  };
+    var img = prompt("Enter a valid image URL: (must include http://):");
+  })
+
+  $("button[id='bold']").click(function(e) {
+    var current_msg = $('textarea').get(0).value;
+
+    var bold = prompt("Enter the text you wish to bold:");
+
+    if( bold != null) {
+      var new_msg = current_msg + "[b]" + bold + "[/b]";
+
+      $('textarea').get(0).value = new_msg;
+    }
+  })
+
+  $("button[id='italicize']").click(function(e) {
+    var current_msg = $('textarea').get(0).value;
+
+    var italicize = prompt("Enter the text you wish to italicize:");
+
+    if( italicize != null) {
+      var new_msg = current_msg + "[i]" + italicize + "[/i]";
+
+      $('textarea').get(0).value = new_msg;
+    }
+  })
+
+  $("button[id='underline']").click(function(e) {
+    var current_msg = $('textarea').get(0).value;
+
+    var underline = prompt("Enter the text you wish to underline:");
+
+    if( underline != null) {
+      var new_msg = current_msg + "[u]" + underline + "[/u]"
+
+      $('textarea').get(0).value = new_msg;
+    }
+  })
+
+  $("button[id='quote']").click(function(e) {
+    var current_msg = $('textarea').get(0).value;
+
+    var quote = prompt("Enter the text you wish to quote:");
+  })
+
+  function printProperties(object) {
+    var property;
+
+    for (property in object) {
+      console.log("Name: " + property);
+      console.log("Value: " + object[property]);
+    }
+  }
 });
