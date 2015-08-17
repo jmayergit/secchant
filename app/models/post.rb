4f1664b9a99d6aeceabafbbde406f0c4fc5bb48d
Julien Mayer <jalexismayer@gmail.com>
2015-08-17 11:06:44 -0500

    adds find page method

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,30 @@ class Post < ActiveRecord::Base
       replies += ",#{reply_id}"
     end
   end
+
+  def self.create_description(post)
+    post.created_at.to_s + "\n" + post.user.user_name
+  end
+
+  '''
+  Finds a posts index relative to other posts in topic
+
+  Used in Forum show page to navigate to last post and
+  in User posts index to navigate to a post that is not
+  necessarily last -- requiring iteration
+  '''
+  def self.find_page(aPost)
+    posts = aPost.topic.posts.order(:created_at)
+    i = 0
+    for post in posts
+      if post == aPost
+        break
+      else
+        i += 1
+      end
+    end
+
+    page = (i / self.per_page) + 1
+    return page
+  end
 end
