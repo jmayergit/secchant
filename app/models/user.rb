2bf4d9312e621508b34c13165204bc7aa171bacc
Julien Mayer <jalexismayer@gmail.com>
2015-08-18 17:27:38 -0700

    adds get_semantic

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,58 @@ class User < ActiveRecord::Base
   validates_presence_of :user_name
   validates_uniqueness_of :user_name
 
-  def self.format_date(datetime)
-    datetime = datetime.to_s
-    regex = /[[:digit:]]{4}-{1}[[:digit:]]{2}-[[:digit:]]{2}/
-    regex.match(datetime).to_s
+
+
+
+  '''
+  Extracts the month as Fixnum from a DateTime instance
+  '''
+  def self.parse_month(datetime)
+    datetime = datetime.to_s if datetime.class != String
+    regex = /-{1}[[:digit:]]{2}/
+    matchData = regex.match(datetime).to_s
+
+    if matchData[1] == "0"
+      month = matchData[2].to_i
+    elsif matchData[1] != "0"
+      month = matchData[1..-1].to_i
+    else
+      "What!"
+    end
   end
+
+
+  '''
+  Parses the year as String from a DateTime instance
+  '''
+  def self.parse_year(datetime)
+    datetime = datetime.to_s if datetime.class != String
+    regex = /[[:digit:]]{4}/
+    matchData = regex.match(datetime)
+
+    year = matchData.to_s
+  end
+
+
+  '''
+  Takes the parameter of a DateTime instance (in this case
+  the created_at attribute of any given User) and converts
+  to a more semantic description -- e.g. Member since August 2015
+  '''
+  def self.get_semantic(datetime)
+    datetime = datetime.to_s
+
+    months = ["January", "Febuary", "March", "April",
+      "May", "June", "July", "August", "September",
+      "October", "November", "December"]
+
+    index = parse_month(datetime)
+    month = months[index]
+
+    year = parse_year(datetime)
+
+    "Member since " + month + " " + year
+  end
+
+
 end
