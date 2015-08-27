9b67f784da6f54dcefb063540ff68cac679bd886
Julien Mayer <jalexismayer@gmail.com>
2015-08-27 08:05:39 +0200

    adds user authorization to comments create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :require_login, only: [:edit, :create]
+
   def create
     @art = Art.find(params[:comment][:art_id])
     @comment = @art.comments.new(comment_params)
@@ -15,5 +17,13 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:text)
+    end
+
+    def require_login
+      unless user_signed_in?
+        flash[:error] = "You must be logged in to comment"
+
+        redirect_to new_user_session_path
+      end
     end
 end
