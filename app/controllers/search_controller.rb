f9816d6fc23836fbb0bfa2f5e524f37e64d9cb54
Julien Mayer <jalexismayer@gmail.com>
2015-08-28 10:01:31 +0200

    modifies views

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,12 @@ class SearchController < ApplicationController
   def topic
     @user = User.find(params[:user])
 
-    @topics = @user.topics
+    @topics = @user.topics.page(params[:page])
   end
 
   def post
     @user = User.find(params[:user])
 
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page])
   end
 end
